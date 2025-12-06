ba03d1af47b119eb8b81442ecea91e31ca211b53
Shashank <shashank@MacBookPro.homenet.telecomitalia.it>
2025-12-06 02:09:26 +0100

    Change team photo background to dark theme on mobile

--- src/components/Sections.jsx
+++ src/components/Sections.jsx
@@ -34,7 +34,7 @@ export default function Sections() {
                         </div>
                     </div>
                 </div>
-                <div className="relative h-[400px] bg-gray-200">
+                <div className="relative h-[400px] bg-[#051C2C]">
                     <img src="/team-photo.jpg" alt="ESCP Finance Society Team" className="absolute inset-0 w-full h-full object-contain md:object-cover grayscale hover:grayscale-0 transition-all duration-500" />
                 </div>
             </section>
